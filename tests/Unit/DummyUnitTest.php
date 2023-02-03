83bfc96b34d2d6770c03180a4cbde7f37bb3f5b7
Minhyung Park <urlinee@msn.com>
2023-02-04 00:38:40 +0900

    더미 테스트 파일 추가

---
+++ tests/Unit/DummyUnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Minhyung\Synology\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyUnitTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
